30e22b4a2976f05eb2bc293fbf2b061ec8153c03
yaquelinehoyos <yaqueline150@gmail.com>
2019-03-27 18:52:10 -0500

    Se agregan comentarios al código

--- hilosSecuencial.c
+++ hilosSecuencial.c
@@ -7,9 +7,9 @@
 
 extern int errno;
 
-void saveNumbers(FILE *file, int *vector, int elements){
-    char * line = NULL;
-    size_t len = 0;
+void saveNumbers(FILE *file, int *vector, int elements){    //Este método se encarga de leer
+    char * line = NULL;                                     //los registros línea por línea
+    size_t len = 0;                                         //y almacenarlos en un vector
     ssize_t read;
     int counter = 0;
 
@@ -19,8 +19,8 @@ void saveNumbers(FILE *file, int *vector, int elements){
     }
 }
 
-int countElementsVector(FILE *file){
-    int counter = 0;
+int countElementsVector(FILE *file){    //Este método se encarga de contar 
+    int counter = 0;                    //los elementos de los vectores
     char c;
     while ((c = fgetc(file)) != EOF)
     {
@@ -30,8 +30,8 @@ int countElementsVector(FILE *file){
     return counter;
 }
 
-int multiplyVectors(int *vector1, int *vector2, int elements){
-    int result = 0;
+int multiplyVectors(int *vector1, int *vector2, int elements){  //Este método se encarga de el
+    int result = 0;                                             //producto punto entre dos vectores
     for (int i = 0; i < elements; i++){
         result += vector1[i] * vector2[i];
     }
@@ -39,21 +39,21 @@ int multiplyVectors(int *vector1, int *vector2, int elements){
 }
 
 int main (int argc, char *argv[]){
-    if(argc != 3){
+    if(argc != 3){          //Verificamos que no hayan más o menos argumentos de los necesarios
         printf("Señor usuario, no ingreso los argumentos requeridos \n");
         printf("Error en el llamado: %s \n", strerror(errno));
         exit(errno);
     }else{
         FILE *file1, *file2;
-        if( ((file1 = fopen(argv[1], "r") ) == NULL) || ((file2 = fopen(argv[2], "r") ) == NULL)){
-            printf("Error en el llamado: %s \n", strerror(errno));
+        if( ((file1 = fopen(argv[1], "r") ) == NULL) || ((file2 = fopen(argv[2], "r") ) == NULL)){  //Verificamos que 
+            printf("Error en el llamado: %s \n", strerror(errno));                                  //los archivos existan
             exit(errno);
         }else{
             int elemntsFile1 = countElementsVector(file1);
-            rewind(file1);
+            rewind(file1);  //Reposicionamos el puntero
             int elemntsFile2 = countElementsVector(file2);
-            rewind(file2);
-            if(elemntsFile1 != elemntsFile2){
+            rewind(file2);  //Reposicionamos el puntero
+            if(elemntsFile1 != elemntsFile2){   //Verificamos que los dos vectores tengan el mismo número de elementos
                 printf("El numero de elementos de los vectores no es el mismo \n");
                 printf("Error en el llamado: %s \n", strerror(errno));
                 exit(errno);
@@ -62,20 +62,13 @@ int main (int argc, char *argv[]){
                 int *numbersFile2 = malloc(elemntsFile2*sizeof(int));
                 saveNumbers(file1, numbersFile1, elemntsFile1);
                 saveNumbers(file2, numbersFile2, elemntsFile2);
-                for(int i = 0; i < elemntsFile1; i++){
-                    printf("posicion: %d valor: %d \n", i, numbersFile1[i]);
-                }
-                printf("\n");
-                for(int i = 0; i < elemntsFile2; i++){
-                    printf("posicion: %d valor: %d \n", i, numbersFile2[i]);
-                }
-                fclose(file1);
-                fclose(file2);
-                clock_t begin = clock();
+                fclose(file1);  //Cerramos el archivo
+                fclose(file2);  //Cerramos el archivo
+                clock_t begin = clock();    //Tomamos el tiempo de inicio de la multiplicación
                 int result = multiplyVectors(numbersFile1, numbersFile2, elemntsFile1);
-                clock_t end = clock();
+                clock_t end = clock();      //Tomamos el tiempo de finalización de la multiplicación
                 double time = 0.0;
-                time += (double)(end - begin) / CLOCKS_PER_SEC;
+                time += (double)(end - begin) / CLOCKS_PER_SEC; //Hallamos el tiempo de ejecución de la multiplicación
                 printf("Resultado: %d \n", result);
                 printf("El tiempo de ejecución de la multiplicación es: %f segundos \n", time);
             }
