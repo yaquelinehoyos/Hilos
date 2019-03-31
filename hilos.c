48a38d98b17e649b8e5a2f7510191f6a1d71b9f2
yaquelinehoyos <yaqueline150@gmail.com>
2019-03-31 17:39:55 -0500

    Se mejora método de división de tareas

--- hilos.c
+++ hilos.c
@@ -40,16 +40,64 @@ int countElementsVector(FILE *file){    //Este método se encarga de contar
     return counter;
 }
 
+void divTaskFourThreads(int numberElements, int *vectorToFull){     //Método de apoyo para 
+    int auxi1 = numberElements / 2;                                 //la división de tareas
+    int auxi2 = numberElements - auxi1;                             //para 4 hilos
+    vectorToFull[0] = auxi1 / 2;
+    vectorToFull[1] = auxi1 - vectorToFull[0];
+    vectorToFull[2] = auxi2 / 2;
+    vectorToFull[3] = auxi2 - vectorToFull[2];
+}
+
+void divTaskEightThreads(int numberElements, int *vectorToFull){    //Método de apoyo para
+    int auxi[5];                                                    // la divisón de tareas
+    auxi[0] = numberElements / 2;                                   //para 8 hilos
+    auxi[1] = numberElements - auxi[0];
+    auxi[2] = auxi[0] / 2;
+    auxi[3] = auxi[0] - auxi[2];
+    auxi[4] = auxi[1] / 2;
+    auxi[5] = auxi[1] - auxi[4];
+    int counter = 2;
+    vectorToFull[0] = auxi[2] /2;
+    for(int i = 1; i < 8; i++){
+        if(i % 2 == 0){
+            vectorToFull[i] = auxi[counter] / 2;
+            counter ++;
+        }else{
+            vectorToFull[i] = auxi[counter] - vectorToFull[i - 1];
+        }
+    }
+}
+
+void divTaskSixteenThreads(int numberElements, int *vectorToFull){  //Método de apoyo para
+    int auxi[13];                                                   //la divisón de tareas
+    auxi[0] = numberElements / 2;                                   //para 16 hilos
+    auxi[1] = numberElements - auxi[0];
+    int counter = 0;
+    for(int i = 2; i < 14; i++){
+        if(i % 2 == 0){
+            auxi[i] = auxi[counter] / 2;
+        }else{
+            auxi[i] = auxi[counter] - auxi[i - 1];
+            counter++;
+        }
+    }
+    vectorToFull[0] = auxi[counter] / 2;
+    for(int i = 1; i < 16; i++){
+        if(i % 2 == 0){
+            vectorToFull[i] = auxi[counter] / 2;
+        }else{
+            vectorToFull[i] = auxi[counter] - vectorToFull[i - 1];
+            counter++;
+        }
+    }
+}
+
 void divTask(int threads, int elements, int *vector){   //Este método es el encargado
     switch(threads){                                    //de la divión de tareas
         case 2:                                         //para cada uno de los hilos
-            if(elements % 2 == 0){
-                vector[0] = elements / 2;
-                vector[1] = elements / 2;
-            }else{
-                vector[0] = elements/2;
-                vector[1] = elements - vector[0];
-            }
+            vector[0] = elements/2;
+            vector[1] = elements - vector[0];
             break;
         case 4:
             if(elements % 4 == 0){
@@ -57,12 +105,7 @@ void divTask(int threads, int elements, int *vector){   //Este método es el enc
                     vector[i] = elements/4;
                 }
             }else{
-                int auxi1 = elements / 2;
-                int auxi2 = elements - auxi1;
-                vector[0] = auxi1 / 2;
-                vector[1] = auxi1 - vector[0];
-                vector[2] = auxi2 / 2;
-                vector[3] = auxi2 - vector[2];
+                divTaskFourThreads(elements, vector);
             }
             break;
         case 8:
@@ -71,20 +114,7 @@ void divTask(int threads, int elements, int *vector){   //Este método es el enc
                     vector[i] = elements/8;
                 }
             }else{
-                int auxi1 = elements / 2;
-                int auxi2 = elements - auxi1;
-                int auxi3 = auxi1 / 2;
-                int auxi4 = auxi1 - auxi3;
-                int auxi5 = auxi2 / 2;
-                int auxi6 = auxi2 - auxi5;
-                vector[0] = auxi3 / 2;
-                vector[1] = auxi3 - vector[0];
-                vector[2] = auxi4 / 2;
-                vector[3] = auxi4 - vector[2];
-                vector[4] = auxi5 / 2;
-                vector[5] = auxi5 - vector[4];
-                vector[6] = auxi6 / 2;
-                vector[7] = auxi6 - vector[6];
+                divTaskEightThreads(elements, vector);
             }
             break;
         case 16:
@@ -93,49 +123,20 @@ void divTask(int threads, int elements, int *vector){   //Este método es el enc
                     vector[i] = elements/16;
                 }
             }else{
-                int auxi1 = elements / 2;
-                int auxi2 = elements - auxi1;
-                int auxi3 = auxi1 / 2;
-                int auxi4 = auxi1 - auxi3;
-                int auxi5 = auxi2 / 2;
-                int auxi6 = auxi2 - auxi5;
-                int auxi7 = auxi3 / 2;
-                int auxi8 = auxi3 - auxi7;
-                int auxi9 = auxi4 / 2;
-                int auxi10 = auxi4 - auxi9;
-                int auxi11 = auxi5 / 2;
-                int auxi12 = auxi5- auxi11;
-                int auxi13 = auxi6 / 2;
-                int auxi14 = auxi6 - auxi13;
-                vector[0] = auxi7 / 2;
-                vector[1] = auxi7 - vector[0];
-                vector[2] = auxi8 / 2;
-                vector[3] = auxi8 - vector[2];
-                vector[4] = auxi9 / 2;
-                vector[5] = auxi9 - vector[4];
-                vector[6] = auxi10 / 2;
-                vector[7] = auxi10 - vector[6];
-                vector[8] = auxi11 / 2;
-                vector[9] = auxi11 - vector[8];
-                vector[10] = auxi12 / 2;
-                vector[11] = auxi12 - vector[10];
-                vector[12] = auxi13 / 2;
-                vector[13] = auxi13 - vector[12];
-                vector[14] = auxi14 / 2;
-                vector[15] = auxi14 - vector[14];
+                divTaskSixteenThreads(elements, vector);
             }
             break;
     }
 }
 
-void putPointer(int *tareas, int numHilos, int *pointers){
-    pointers[0] = 0;
-    for(int i = 1; i < numHilos; i++){
+void putPointer(int *tareas, int numHilos, int *pointers){  //Este método se encarga
+    pointers[0] = 0;                                        //de darle posición a los
+    for(int i = 1; i < numHilos; i++){                      //punteros de cada hilo 
         pointers[i] = pointers[i - 1] + tareas[i - 1];
     }
 }
 
-void *threadFunction(void *args){                                       
+void *threadFunction(void *args){         //Método para la multiplicación                              
     struct vectorStruct *actualArgs = args;                                                                 
     for(int i = 0; i < actualArgs->numberTasks; i++){                               
         actualArgs->resultado[actualArgs->idHilo] += (*actualArgs->p1) * (*actualArgs->p2);
@@ -146,7 +147,7 @@ void *threadFunction(void *args){
 }
 
 int main (int argc, char *argv[]){
-    if(argc != 4){      
+    if(argc != 4){      //Verificamos que no hayan más o menos argumentos de los necesarios
         printf("Señor usuario, no ingreso los argumentos requeridos \n");
         printf("Error en el llamado: %s \n", strerror(errno));
         exit(errno);
@@ -154,38 +155,39 @@ int main (int argc, char *argv[]){
         FILE *file1, *file2;
         int numeroHilos = atoi(argv[3]);
         if(numeroHilos != 2 && numeroHilos != 4 && numeroHilos != 8 && numeroHilos != 16){  
-            printf("Error en el llamado: %s \n", strerror(errno));                     
+            printf("El número de hilos debe ser 2, 4, 8 o 16 \n");  //verificamos que el número 
+            printf("Error en el llamado: %s \n", strerror(errno));  //de hilos sea el correcto
             exit(errno);
         }else{
-            if( ((file1 = fopen(argv[1], "r") ) == NULL) || ((file2 = fopen(argv[2], "r") ) == NULL)){  
-            printf("Error en el llamado: %s \n", strerror(errno));                                     
-            exit(errno);
+            if( ((file1 = fopen(argv[1], "r") ) == NULL) || ((file2 = fopen(argv[2], "r") ) == NULL)){  //verificamos que los
+                printf("Error en el llamado: %s \n", strerror(errno));                                  //archivos existan
+                exit(errno);
             }else{
                 int elementsFile1 = countElementsVector(file1);
                 rewind(file1);
                 int elementsFile2 = countElementsVector(file2);
                 rewind(file2);
                 if(elementsFile1 != elementsFile2){     
-                    printf("El numero de elementos de los vectores no es el mismo \n");
-                    printf("Error en el llamado: %s \n", strerror(errno));
+                    printf("El numero de elementos de los vectores no es el mismo \n"); //verificamos que el número de elementos
+                    printf("Error en el llamado: %s \n", strerror(errno));              //de los vectores sean iguales
                     exit(errno);
                 }else{
                     if(numeroHilos > elementsFile1){    
-                        printf("El numero de hilos supera el numero de elementos del vector \n");
-                        printf("Error en el llamado: %s \n", strerror(errno));
-                        exit(errno);
+                        printf("El numero de hilos supera el numero de elementos del vector \n");   //Verificamos que se pueda realizar
+                        printf("Error en el llamado: %s \n", strerror(errno));                      //la operación con el número de hilos
+                        exit(errno);                                                                //requeridos por el usuario
                     }else{
                         int *numbersFile1 = malloc(elementsFile1*sizeof(int));
                         int *numbersFile2 = malloc(elementsFile2*sizeof(int));
                         saveNumbers(file1, numbersFile1, elementsFile1);
                         saveNumbers(file2, numbersFile2, elementsFile2);
-                        struct vectorStruct args[numeroHilos]; 
+                        struct vectorStruct args[numeroHilos];  //Creamos un struct por hilo
                         int tasks[numeroHilos];
                         divTask(numeroHilos, elementsFile1, tasks);
                         int punteros[numeroHilos];
                         putPointer(tasks, numeroHilos, punteros);
                         int *resultadoPorHilo = malloc(numeroHilos*sizeof(int));
-                        for(int i = 0; i < numeroHilos; i++){
+                        for(int i = 0; i < numeroHilos; i++){   //Asignamos valores a cada struct de cada hilo
                             args[i].vectorFile1 = numbersFile1;
                             args[i].vectorFile2 = numbersFile2;
                             args[i].numberTasks = tasks[i];
@@ -194,7 +196,7 @@ int main (int argc, char *argv[]){
                             args[i].p1 = &args[i].vectorFile1[punteros[i]];
                             args[i].p2 = &args[i].vectorFile2[punteros[i]];
                         }
-                        pthread_t threads[numeroHilos];
+                        pthread_t threads[numeroHilos]; 
                         clock_t begin = clock();    
                         for(int i = 0; i < numeroHilos; i++){
                             pthread_create(&threads[i], NULL, threadFunction, &args[i]);  
